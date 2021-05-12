8d9cd981838b1bad94a1724c742394dc77447a0d
brianliang <brianliang@master60.com.tw>
2021-05-12 15:42:34 +0800

    init project

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, send_from_directory
+import os
+scriptPath = os.path.dirname(os.path.realpath(__file__))
+os.chdir(scriptPath) 
 
-app = Flask(__name__)
+template_folder = os.path.join(os.getcwd(),'templates')
+static_folder = os.path.join(os.getcwd(),'static')
+app = Flask(__name__, template_folder=template_folder,static_folder=static_folder)
 
-@app.route('/')
-def hello():
-    return "yee"
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path,mimetype='text/css')
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 if __name__ == 'main':
     app.run()
